30ac5bf4f17401b639e6398b8df3ba5a7110e885
JelencsikTibi <jelencsiktibi@gmail.com>
2021-10-01 21:27:20 +0200

    Add delete function

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -23,6 +23,7 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
             button2.Text = Resource1.WriteToFile;
+            button3.Text = Resource1.Delete;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
@@ -47,10 +48,15 @@ namespace UserMaintenance
             {
                 foreach (var u in users)
                     sw.WriteLine(string.Format(
-                        "(0);(1)",
+                        "{0};{1}",
                         u.ID,
                         u.FullName)); 
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            users.Remove((User)listBox1.SelectedItem);
         }
     }
 }
