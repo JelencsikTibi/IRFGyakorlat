39a3905b8b36f99187ded4a1079b5a0e0defcac1
JelencsikTibi <jelencsiktibi@gmail.com>
2021-10-29 13:00:25 +0200

    Fill Datagridview

--- Webszolgaltatas/Webszolgaltatas/Form1.cs
+++ Webszolgaltatas/Webszolgaltatas/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Webszolgaltatas.Entities;
 using Webszolgaltatas.MnbServiceReference;
 
@@ -21,11 +22,36 @@ namespace Webszolgaltatas
         public Form1()
         {
             InitializeComponent();
-            Consume();
+            string xmlstring = Consume();
+            LoadXml(xmlstring);
             dataGridView1.DataSource = Rates;
+
         }
 
-        void Consume()
+        private void LoadXml(string input)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(input);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                RateData rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+                
+            }
+        }
+
+        string Consume()
         {
             MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
             GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
@@ -34,7 +60,7 @@ namespace Webszolgaltatas
             request.endDate = "2020-06-30";
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
-            File.WriteAllText("export.xml", result);
+            return result;
         }
     }
 }
