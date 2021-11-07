784c7ce5307908450cfb9366a0cde75760fd23da
JelencsikTibi <jelencsiktibi@gmail.com>
2021-11-07 19:02:10 +0100

    Added toy selector buttons

--- Karacsony/Karacsony/Form1.Designer.cs
+++ Karacsony/Karacsony/Form1.Designer.cs
@@ -33,6 +33,9 @@ namespace Karacsony
             this.mainPanel = new System.Windows.Forms.Panel();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.carButton = new System.Windows.Forms.Button();
+            this.ballButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -56,15 +59,48 @@ namespace Karacsony
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick);
             // 
+            // carButton
+            // 
+            this.carButton.Location = new System.Drawing.Point(12, 12);
+            this.carButton.Name = "carButton";
+            this.carButton.Size = new System.Drawing.Size(75, 50);
+            this.carButton.TabIndex = 1;
+            this.carButton.Text = "CAR";
+            this.carButton.UseVisualStyleBackColor = true;
+            this.carButton.Click += new System.EventHandler(this.carButton_Click);
+            // 
+            // ballButton
+            // 
+            this.ballButton.Location = new System.Drawing.Point(93, 12);
+            this.ballButton.Name = "ballButton";
+            this.ballButton.Size = new System.Drawing.Size(75, 50);
+            this.ballButton.TabIndex = 2;
+            this.ballButton.Text = "BALL";
+            this.ballButton.UseVisualStyleBackColor = true;
+            this.ballButton.Click += new System.EventHandler(this.ballButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Coming Next :";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ballButton);
+            this.Controls.Add(this.carButton);
             this.Controls.Add(this.mainPanel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,6 +109,9 @@ namespace Karacsony
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button carButton;
+        private System.Windows.Forms.Button ballButton;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Karacsony/Karacsony/Form1.cs
+++ Karacsony/Karacsony/Form1.cs
@@ -14,14 +14,28 @@ namespace Karacsony
 {
     public partial class Form1 : Form
     {
-        
+        private Toy _nextToy;
 
         private List<Toy> _toys = new List<Toy>();
         private IToyFactory _factory;
         public IToyFactory Factory 
         {
           get { return _factory; }
-          set { _factory = value; }
+          set 
+            { 
+                _factory = value;
+                DisplayNext();
+            }
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
         }
 
         public Form1()
@@ -56,6 +70,16 @@ namespace Karacsony
                 mainPanel.Controls.Remove(oldestToy);
                 _toys.Remove(oldestToy);
             }
+        }
+
+        private void carButton_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void ballButton_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
         }
     }
 }
