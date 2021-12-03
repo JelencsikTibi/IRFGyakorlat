4ec65820a3ca89b66823de0d2073facba7a85920
JelencsikTibi <jelencsiktibi@gmail.com>
2021-12-03 10:07:53 +0100

    Created 4 classes

---
+++ Mikroszimuláció/Mikroszimuláció/Entities/BirthProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimuláció.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+
+
+        
+    }
+}
